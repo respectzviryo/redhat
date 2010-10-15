c9b21c7e2a6b5ef7de9d4cb3e499ec981b6def26
eugenek <respectzviryo@yandex.ru>
2010-10-15 15:15:26 +0300

    added scope to find all categories with status new

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ActiveRecord::Base
   belongs_to :category
 
   named_scope :finished, :conditions => {:status => "finished"}
+  named_scope :new, :conditions => {:status => "new"}
   named_scope :started, :conditions => "status != 'finished'"
   named_scope :by_category, lambda {|category_id| {:conditions => {:category_id => category_id}}}
 
